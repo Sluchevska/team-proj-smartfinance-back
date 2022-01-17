356f763b3ff31f7461f0cd573b83b80464e05903
Andrii Shovkun <andrii.shovkun@gmail.com>
2022-01-17 23:22:35 +0200

    separate res for two arrives

--- controllers/operations/getAllDayTransacthios.js
+++ controllers/operations/getAllDayTransacthios.js
@@ -4,21 +4,26 @@ getAllDayTransacthios = async (req, res) => {
   const { date } = req.query;
   const { id } = req.user;
 
-  const transactions = await Operation.find({ owner: id, date });
-
-  if (transactions.length === 0) {
-    res.status(404).json({
-      status: "Not found",
-      code: 404,
-    });
-
-    return;
-  }
+  const transactionsIncome = await Operation.find({
+    owner: id,
+    date,
+    type: "income",
+  });
+  const transactionsExpenses = await Operation.find({
+    owner: id,
+    date,
+    type: "expenses",
+  });
 
   res.status(200).json({
     status: "success",
     code: 200,
-    data: transactions,
+    data: {
+      transactions: {
+        income: transactionsIncome,
+        expenses: transactionsExpenses,
+      },
+    },
   });
 };
 
