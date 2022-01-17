3d4b3a84f0e56bcc73d15fe649a520360e8222c6
TarIryna <tar86irina@gmail.com>
2022-01-17 13:15:05 +0200

    required type added

--- models/operation.js
+++ models/operation.js
@@ -41,7 +41,7 @@ const operationJoiSchema = Joi.object({
   date: Joi.object().required(),
   category: Joi.string().required(),
   description: Joi.string(),
-  type: Joi.string().valid("income", "expenses"),
+  type: Joi.string().valid("income", "expenses").required(),
   sum: Joi.number()
 });
 
