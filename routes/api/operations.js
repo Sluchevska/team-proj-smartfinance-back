719e4e36f66a4386f813ddb190cad3c15a1ef92d
Andrii Shovkun <andrii.shovkun@gmail.com>
2022-01-19 13:29:56 +0200

    change res for one type of operations

--- routes/api/operations.js
+++ routes/api/operations.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { auth, ctrlWrapper } = require('../../middlewares');
-const { operations: ctrl } = require('../../controllers');
+const { auth, ctrlWrapper } = require("../../middlewares");
+const { operations: ctrl } = require("../../controllers");
 
-router.post("/balance", auth, ctrlWrapper(ctrl.submitBalance));
+router.post("/balance", auth, ctrlWrapper(ctrl.setBalance));
 router.get("/info", auth, ctrlWrapper(ctrl.getAllInfo));
 router.get("/byday", auth, ctrlWrapper(ctrl.getOperationsByDay));
 router.post("/", auth, ctrlWrapper(ctrl.addOperation));
 router.delete("/:operationId", auth, ctrlWrapper(ctrl.removeOperation));
-router.get('/getExpenseByMonth', auth, ctrlWrapper(ctrl.getExpenseByMonth));
-router.get('/', auth, ctrlWrapper(ctrl.getIncomeExpenseByMonth));
+router.get("/getExpenseByMonth", auth, ctrlWrapper(ctrl.getExpenseByMonth));
+router.get("/", auth, ctrlWrapper(ctrl.getIncomeExpenseByMonth));
 
 module.exports = router;
