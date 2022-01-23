7fcc2d32ac191b1fa891081fde189f287ac85106
Andrey Shemet <an-shem@ukr.net>
2022-01-23 05:12:11 +0200

    Поправил ответ по current useru

--- controllers/users/userGetCurrent.js
+++ controllers/users/userGetCurrent.js
@@ -2,13 +2,13 @@ const { getCurrent } = require('../../services/users');
 
 const userGetCurrent = async (req, res) => {
   const { id } = req.user;
-  const { name, email, balance } = await getCurrent(id);
+  const { name, email } = await getCurrent(id);
 
   res.status(200).json({
     status: 'Ok',
     code: 200,
     data: {
-      user: { name, email, balance },
+      user: { name, email },
     },
   });
 };
