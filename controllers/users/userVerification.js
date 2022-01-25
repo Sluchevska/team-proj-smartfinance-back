6ff11152269e8d790961fb0d2c40e6bd4f563201
Andrey Shemet <an-shem@ukr.net>
2022-01-25 04:36:58 +0200

    Подправил верификацию чтоб сразу перебрасовало на сайт
    - сделал так чтоб когда подтверждаешь почту  то сразу попадал на свой сайт.

--- controllers/users/userVerification.js
+++ controllers/users/userVerification.js
@@ -2,8 +2,8 @@ const { verificationByToken } = require('../../services/users');
 
 const userVerification = async (req, res) => {
   const { verificationToken } = req.params;
-  await verificationByToken(verificationToken);
-  res.redirect('https://team-proj-smartfinance.netlify.app/home?verify=true');
+  const { token } = await verificationByToken(verificationToken);
+  res.redirect(`https://team-proj-smartfinance.netlify.app/home?verify=true&access_token=${token}`);
 };
 
 module.exports = { userVerification };
--- services/users/verificationByToken.js
+++ services/users/verificationByToken.js
@@ -1,16 +1,20 @@
-const { User } = require("../../models/user");
-const { NotFound} = require("http-errors");
+const jwt = require('jsonwebtoken');
+const { User } = require('../../models/user');
+const { NotFound } = require('http-errors');
 
 const verificationByToken = async (verifyToken) => {
-    const user = await User.findOne({ verifyToken });
-    if (!user) {
-      throw new NotFound("User is verificated or does not exist");
-    }
-    const result = await User.findOneAndUpdate(
-      { _id: user._id },
-      { $set: { verify: true, verifyToken: null } }
-    );
-    return result;
-  };
+  const user = await User.findOne({ verifyToken });
+  if (!user) {
+    throw new NotFound('User is verificated or does not exist');
+  }
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET
+  );
+  const result = await User.findOneAndUpdate({ _id: user._id }, { $set: { verify: true, verifyToken: null, token } });
+  return result;
+};
 
-  module.exports = { verificationByToken }
+module.exports = { verificationByToken };
